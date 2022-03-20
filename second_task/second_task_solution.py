28321ec97a13d9d194a7fb81819c9886a89e3f02
IliaSharkin <python3633@gmail.com>
2022-03-21 00:31:26 +0300

    first_commit

--- second_task/second_task_solution.py
+++ second_task/second_task_solution.py
@@ -1,11 +1,12 @@
 import os
 import pprint
 
+
 class MatchManager:
     def __init__(self, path) -> None:
         self.path = path
-    
-    def _get_matched_files_list(self, dir:str) -> list:
+
+    def _get_matched_files_list(self, dir: str) -> list:
         '''Get directory name and return matched file list in it'''
         matched_files_list = []
         files = os.listdir(dir)
@@ -16,24 +17,27 @@ class MatchManager:
                 if match_file == file:
                     continue
                 if filename == match_file_name and match_file_extention.lower() == '.json':
-                    matched_files_list.append([os.path.join(self.path, dir, file), os.path.join(self.path, dir, match_file)])
+                    matched_files_list.append([
+                        os.path.join(self.path, dir, file),
+                        os.path.join(self.path, dir, match_file)
+                    ])
         return matched_files_list
-        
-        
+
     def print_matched_dict(self) -> None:
         '''Print matched dictionary'''
         matched_dict = {}
         files = os.listdir(self.path)
         dirs = []
         for file in files:
-            if os.path.isdir(os.path.join(self.path, file)): dirs.append(os.path.join(self.path, file))
+            if os.path.isdir(os.path.join(self.path, file)):
+                dirs.append(os.path.join(self.path, file))
         dirs.sort()
         for dir in dirs:
             if self._get_matched_files_list(dir):
                 matched_dict[os.path.basename(dir)] = self._get_matched_files_list(dir)
         pprint.pprint(matched_dict)
-    
-            
+
+
 if __name__ == '__main__':
     mm = MatchManager("/tmp/labels")
     mm.print_matched_dict()
